3a7c32ee77e567b63ae55d5ea891c1164fba8ba5
Yiru Jeon <yiru.jeon@gmail.com>
2022-04-09 09:30:19 +0900

    초창기 버전 commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple-online-status
 simple flask server to share my online status
+
+- Pavlov VR 게임 상태 공유용
+- 사용자로부터 ID만 수집받기에 SSL 사용하지 않음
+
+# ToDo
+- [ ] string input sanitize
---
+++ index.py
@@ -0,0 +1,61 @@
+import json
+from flask import Flask, request, render_template, redirect, make_response, url_for
+import datetime
+import pytz
+
+app = Flask(__name__)
+
+status = {}
+
+
+@app.route("/join", methods=["POST"])
+def join():
+    if 'id' not in request.cookies:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        id = request.cookies['id']
+        playnote = request.form['playnote']
+        now = datetime.datetime.now().astimezone(pytz.timezone('Asia/Seoul')).time().strftime("%H:%M:%S")
+        end_time = int(request.form['end_time'])
+        status[id] = (playnote, now, end_time)
+    else:
+        return "ERROR, 개발자에게 문의하세요"
+
+    return redirect(url_for('index'))
+
+@app.route("/exit", methods=["POST"])
+def exit():
+    if 'id' not in request.cookies:
+        return redirect(url_for('index'))
+    if request.method == "POST":
+        id = request.cookies['id']
+        if id not in status:
+            return "ERROR, 쿠키 멋대로 바꾸셨나요? 개발자에게 문의하세요"
+        del(status[id])
+    else:
+        return "ERROR, 개발자에게 문의하세요"
+
+    return redirect(url_for('index'))
+
+
+@app.route("/setcookie", methods=["POST"])
+def setcookie():
+    id = request.form['id']
+    resp = make_response(redirect(url_for('index')))
+    if len(id) > 0:
+      resp.set_cookie('id', id)
+      # id 길이가 0 인건 제외해버림
+    return resp
+
+
+@app.route("/")
+def index():
+    if 'id' in request.cookies:
+        return render_template('index.html', status=status, id=request.cookies['id'])
+    else:
+        return render_template('request_id.html')
+
+
+
+
+app.run(host="0.0.0.0", port=8080)
